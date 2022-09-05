49793b234bc23cebd68c075539f3170c25936eea
YoungSoulluoS <70120313+YoungSoulluoS@users.noreply.github.com>
2022-09-05 21:19:40 +0500

    -added Vex compatible

---
+++ src/main/java/net/dorianpb/cem/external/models/CemVexModel.java
@@ -0,0 +1,30 @@
+package net.dorianpb.cem.external.models;
+
+import net.dorianpb.cem.internal.api.CemModel;
+import net.dorianpb.cem.internal.models.CemModelRegistry;
+import net.dorianpb.cem.internal.models.CemModelRegistry.CemPrepRootPartParamsBuilder;
+import net.minecraft.client.render.entity.model.VexEntityModel;
+import net.minecraft.entity.mob.VexEntity;
+
+import java.util.*;
+
+public class CemVexModel extends VexEntityModel implements CemModel{
+	private static final Map<String, String>       partNames  = new HashMap<>();
+	private static final Map<String, List<String>> familyTree = new LinkedHashMap<>();
+	private final        CemModelRegistry          registry;
+	
+	public CemVexModel(CemModelRegistry registry){
+		super(registry.prepRootPart((new CemPrepRootPartParamsBuilder()).setPartNameMap(partNames)
+		                                                                .setFamilyTree(familyTree)
+		                                                                .setVanillaReferenceModelFactory(() -> getTexturedModelData().createModel())
+		                                                                .create()));
+		this.registry = registry;
+		this.rotatePart(this.registry.getEntryByPartName("body"), 'x', 90);
+	}
+	
+	@Override
+	public void setAngles(VexEntity entity, float limbAngle, float limbDistance, float animationProgress, float headYaw, float headPitch){
+		super.setAngles(entity, limbAngle, limbDistance, animationProgress, headYaw, headPitch);
+		this.registry.applyAnimations(limbAngle, limbDistance, animationProgress, headYaw, headPitch, entity);
+	}
+}
